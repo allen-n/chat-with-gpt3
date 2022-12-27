52aefde43f9e6ccdeaec9901b9ec166eb0034dea
Allen Nikka <allennikka@gmail.com>
2022-12-26 19:28:58 -0800

    Getting completion responses back from gpt3

--- src/pages/api/tts.ts
+++ src/pages/api/tts.ts
@@ -3,6 +3,34 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { getServerAuthSession } from "../../server/common/get-server-auth-session";
 import { speechToTextQuery, type SpeechToTextModelResp } from "../../utils/tts";
 import { base64ToBlob } from "../../utils/encoding";
+
+import { Configuration, OpenAIApi, CreateCompletionResponse } from "openai";
+
+const CompletionRequest = async (prompt: string) => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_SK,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  // TODO allen: will need to prompt engineer a bit to keep context of the conversation
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-curie-001", // "text-davinci-003", more performant but 10x the price
+      prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: ${prompt}?\nAI: `,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
+    });
+    return completion;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
 // TODO allen Maybe this should be in a types file somehwere?
 export type SpeechToTextRequest = {
   b64FileString: string; // base64 encoded string
@@ -28,7 +56,17 @@ const restricted = async (req: NextApiRequest, res: NextApiResponse) => {
       modelResp: result,
       index: index,
     };
-    console.log("speechToTextResponse", resp);
+    if (typeof resp.modelResp.text !== "undefined") {
+      const completion = await CompletionRequest(resp.modelResp.text);
+      console.log("completion status", completion?.status);
+      console.log("completion usage", completion?.data.usage);
+      const choice = completion?.data.choices[0]?.text;
+
+      console.log(
+        "completion choice",
+        choice || "I'm at a loss for words, sorry!"
+      );
+    }
     res.send(resp);
   } else {
     res.send({
